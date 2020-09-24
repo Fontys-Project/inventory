402c9a738bc961194ac227a304af69ad7f14ab77
Mark Waltmans <mfpwaltmans@live.nl>
2020-09-24 23:14:50 +0200

    Add first classes

---
+++ InventoryLogic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InventoryLogic
+{
+    public class Class1
+    {
+    }
+}
---
+++ InventoryLogic/Product/IProductDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryLogic.Product
+{
+    interface IProductDAO
+    {
+        IProduct GetProduct(int ID);
+        List<IProduct> GetAllProducts();
+        void AddProduct(IProduct product);
+        void RemoveProduct(int ID);
+        void ModifyProduct(IProduct product);
+
+    }
+}
