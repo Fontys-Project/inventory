b066e41f067653939474d7aa17349f1e25f3729b
Teun Cortooms <teuncortooms@gmail.com>
2020-10-25 23:02:20 +0100

    Id setter needed for Swagger

--- InventoryLogic/Stocks/Stock.cs
+++ InventoryLogic/Stocks/Stock.cs
@@ -5,7 +5,7 @@ namespace InventoryLogic.Stocks
 {
     public class Stock
     {
-        public int Id { get; }
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public Product Product { get; set; }
         public int Amount { get; set; }
