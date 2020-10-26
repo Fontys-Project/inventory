5b11790d8c4edadb2d2aeb3389660294fd7aa55a
Teun Cortooms <teuncortooms@gmail.com>
2020-10-26 14:51:32 +0100

    Add generic get method to factory

--- InventoryLogic/Facade/IDatabaseFactory.cs
+++ InventoryLogic/Facade/IDatabaseFactory.cs
@@ -1,14 +1,16 @@
 ﻿using InventoryLogic.Products;
 using InventoryLogic.Tags;
 using InventoryLogic.Stocks;
+using InventoryLogic.ProductTagJoins;
 
 namespace InventoryLogic.Facade
 {
     public interface IDatabaseFactory
     {
-        public IProductDAO ProductDAO { get; }
-        public ITagDAO TagDAO { get; }
-        public IStockDAO StockDAO { get; }
+        public ICrudDAO<Product> ProductDAO { get; }
+        public ICrudDAO<Tag> TagDAO { get; }
+        public ICrudDAO<Stock> StockDAO { get; }
+        public ICrudDAO<ProductTagJoin> ProductTagJoinDAO { get; }
         public ICrudDAO<T> GetCrudDAO<T>();
     }
 }
