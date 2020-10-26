3cc1e6bfda189d171845a59f86e169963c8be8fa
Teun Cortooms <teuncortooms@gmail.com>
2020-10-26 14:56:26 +0100

    DAOs for join table

---
+++ InventoryDAL/ProductTagJoins/ProductTagJoinMockDAO.cs
@@ -0,0 +1,63 @@
+﻿using InventoryLogic.Facade;
+using InventoryLogic.Products;
+using InventoryLogic.ProductTagJoins;
+using InventoryLogic.Tags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InventoryDAL.ProductTagJoins
+{
+    public class ProductTagJoinMockDAO : ICrudDAO<ProductTagJoin>
+    {
+        private readonly List<ProductTagJoin> joins;
+
+        public ProductTagJoinMockDAO()
+        {
+            joins = new List<ProductTagJoin>
+            {
+                new ProductTagJoin{
+                    ProductId = 1,
+                    Product = new Product(1, "name", 25, "sku"),
+                    TagId = 1,
+                    Tag = new Tag(1, "tagname")
+                }
+            };
+        }
+
+        public void Add(ProductTagJoin join)
+        {
+            joins.Add(join);
+        }
+
+        public List<ProductTagJoin> GetAll()
+        {
+            return joins;
+        }
+
+        public ProductTagJoin Get(int joinId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProductTagJoin Get(int productId, int tagId)
+        {
+            return joins.Where(j => j.ProductId == productId && j.TagId == tagId).First();
+        }
+
+        public void Modify(ProductTagJoin join)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int joinId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int productId, int tagId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InventoryDAL/ProductTagJoins/ProductTagJoinMySQLDAO.cs
@@ -0,0 +1,70 @@
+﻿using InventoryLogic.Products;
+using InventoryDAL.Database;
+using InventoryLogic.Facade;
+using InventoryLogic.ProductTagJoins;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace InventoryDAL.ProductTagJoins
+{
+    public class ProductTagJoinMySQLDAO : ICrudDAO<ProductTagJoin>
+    {
+        private readonly MySqlContext dbContext;
+        private DbSet<ProductTagJoin> Table { get; set; }
+
+        public ProductTagJoinMySQLDAO(MySqlContext context)
+        {
+            this.dbContext = context;
+            this.Table = context.Set<ProductTagJoin>();
+        }
+
+        public void Add(ProductTagJoin obj)
+        {
+            this.dbContext.Database.EnsureCreated();
+            this.Table.Add(obj);
+            this.dbContext.SaveChangesAsync();
+        }
+
+        public List<ProductTagJoin> GetAll()
+        {
+            this.dbContext.Database.EnsureCreated();
+            Task<List<ProductTagJoin>> lst = this.Table.ToListAsync();
+            lst.Wait(); // TODO: beter async uitwerken?
+            return lst.Result;
+        }
+
+        public ProductTagJoin Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProductTagJoin Get(int productId, int tagId)
+        {
+            this.dbContext.Database.EnsureCreated();
+            return this.Table.Where(j => j.ProductId == productId && j.TagId == tagId).FirstOrDefault();
+        }
+
+        public void Modify(ProductTagJoin obj)
+        {
+            this.dbContext.Database.EnsureCreated();
+            this.dbContext.Update(obj);
+            this.dbContext.SaveChangesAsync();
+        }
+
+        public void Remove(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Remove(int productId, int tagId)
+        {
+            this.dbContext.Database.EnsureCreated();
+            this.Table.Remove(
+                this.Table.Where(j => j.ProductId == productId && j.TagId == tagId).FirstOrDefault()
+                );
+            this.dbContext.SaveChangesAsync();
+        }
+    }
+}
