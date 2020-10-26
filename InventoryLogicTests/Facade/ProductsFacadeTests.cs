cd5f14271ef708edd07160a76dd6205fde034013
Teun Cortooms <teuncortooms@gmail.com>
2020-10-26 20:02:54 +0100

    Fix bugs and add test templates

---
+++ InventoryLogicTests/Facade/ProductsFacadeTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InventoryLogic.Facade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryLogic.Facade.Tests
+{
+    [TestClass()]
+    public class ProductsFacadeTests
+    {
+        [TestMethod()]
+        public void ProductsFacadeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
