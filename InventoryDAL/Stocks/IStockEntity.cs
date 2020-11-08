94216361258109812d6de339a7676ddd1b55c797
Teun Cortooms <teuncortooms@gmail.com>
2020-11-08 23:40:30 +0100

    Add interfaces (to be used by builders)

---
+++ InventoryDAL/Stocks/IStockEntity.cs
@@ -0,0 +1,14 @@
+﻿using InventoryDAL.Products;
+using System;
+
+namespace InventoryDAL.Stocks
+{
+    public interface IStockEntity
+    {
+        int Amount { get; set; }
+        DateTime Date { get; set; }
+        int Id { get; set; }
+        ProductEntity ProductEntity { get; set; }
+        int ProductId { get; set; }
+    }
+}
---
+++ InventoryLogic/Stocks/IStock.cs
@@ -0,0 +1,14 @@
+﻿using InventoryLogic.Products;
+using System;
+
+namespace InventoryLogic.Stocks
+{
+    public interface IStock
+    {
+        int Amount { get; set; }
+        DateTime Date { get; set; }
+        int Id { get; set; }
+        Product Product { get; set; }
+        int ProductId { get; set; }
+    }
+}
