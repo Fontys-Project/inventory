dd629691855f462a7127e4a4ec3540217038671f
Teun Cortooms <teuncortooms@gmail.com>
2020-11-12 17:01:13 +0100

    Includes (don't work)

--- InventoryDAL/Products/DAO/ProductEntityMySQLDAO.cs
+++ InventoryDAL/Products/DAO/ProductEntityMySQLDAO.cs
@@ -30,6 +30,7 @@ namespace InventoryDAL.Products
             this.dbContext.Database.EnsureCreated();
             Task<List<ProductEntity>> lst = this.ProductEntityTable
                 .Include(pe => pe.ProductTagEntities)
+                .Include(pe => pe.StockEntities)
                 .ToListAsync(); 
             lst.Wait();
             return lst.Result;
