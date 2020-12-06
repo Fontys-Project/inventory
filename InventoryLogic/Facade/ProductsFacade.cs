be7a533d3b22415670da8905057d8b3bc729f2e3
Teun Cortooms <teuncortooms@gmail.com>
2020-12-06 20:46:43 +0100

    update

--- InventoryLogic/Facade/ProductsFacade.cs
+++ InventoryLogic/Facade/ProductsFacade.cs
@@ -16,21 +16,21 @@ namespace InventoryLogic.Facade
         }
 
 
-        //public override List<ProductDTO> GetAll() // TODO: remove
-        //{
-        //    busMessenger.Publish(new Message { Text = "lalalalala" });
+        public override List<ProductDTO> GetAll() // TODO: remove
+        {
+            busMessenger.Publish(new Message { Text = "lalalalala" });
 
-        //    List<ProductDTO> newViews = new List<ProductDTO>();
-        //    List<Product> records = repoFactory.GetCrudRepository<Product>().GetAll();
+            List<ProductDTO> newViews = new List<ProductDTO>();
+            List<Product> records = repoFactory.GetCrudRepository<Product>().GetAll();
 
-        //    foreach (Product record in records)
-        //    {
-        //        ProductDTO newView = new ProductDTO();
-        //        record.ConvertToDTO(newView);
-        //        newViews.Add(newView);
-        //    }
+            foreach (Product record in records)
+            {
+                ProductDTO newView = new ProductDTO();
+                record.ConvertToDTO(newView);
+                newViews.Add(newView);
+            }
 
-        //    return newViews;
-        //}
+            return newViews;
+        }
     }
 }
