16212d9400fe990a6ed9a052260e16d79a5fb61d
R. de Jong <rdejong81@gmail.com>
2021-01-11 20:12:15 +0100

    Progress bars wijziging o.a.

--- IntegratieTests/test1.ps1
+++ IntegratieTests/test1.ps1
@@ -4,6 +4,8 @@ $inventorybranch = "IntegratieTests"
 $gitdirs = @(($env:TEMP + "/inventorysvc"),($env:TEMP + "/loginsvc"))
 $workdirs = @(($env:TEMP + "/inventorysvc"),($env:TEMP + "/loginsvc/LoginService"))
 
+Write-Progress -PercentComplete 0 -Activity 'Integration test: Login to Inventory, to create a product' -Status 'Cloning git repositories'
+
 if($null -eq (Get-Module -Name posh-git))
 {
   Install-Module posh-git -Scope CurrentUser -Force
@@ -19,38 +21,56 @@ foreach($dir in $gitdirs)
 }
 
 # login token script
-git clone -q -b $loginbranch https://github.com/Fontys-Project/login.git $env:TEMP/loginsvc
-Write-Host "Cloned Login repo"
-git clone -q -b $inventorybranch https://github.com/Fontys-Project/inventory.git $env:TEMP/inventorysvc
-Write-Host "Cloned Inventory repo"
+Write-Progress -PercentComplete 1 -Activity 'Integration test: Login to Inventory, to create a product' -Status 'Cloning git repositories: loginsvc'
+start-process -Wait -FilePath "git" -ArgumentList @("clone", "-q", "-b", "$loginbranch", "https://github.com/Fontys-Project/login.git", "$env:TEMP/loginsvc")
+Write-Progress -PercentComplete 5 -Activity 'Integration test: Login to Inventory, to create a product' -Status 'Cloning git repositories: inventorysvc'
+start-process -Wait -FilePath "git" -ArgumentList @("clone", "-q", "-b", "$inventorybranch", "https://github.com/Fontys-Project/inventory.git", "$env:TEMP/inventorysvc")
+
+$state = 10
 
 foreach($dir in $workdirs)
 {
-cd $dir
-docker-compose build
+Write-Progress -PercentComplete $state -Activity 'Integration test: Login to Inventory, to create a product' -Status "Docker compose: build service $dir"
 
-docker-compose up -d
+cd $dir
+start-process -Wait -FilePath "docker-compose" -ArgumentList @("build")
+$state += 10
+
+Write-Progress -PercentComplete $state -Activity 'Integration test: Login to Inventory, to create a product' -Status "Docker compose: up service $dir"
+start-process -Wait -FilePath "docker-compose" -ArgumentList @("up", "-d")
+
+#docker-compose up -d
 start-sleep -Seconds 20
+$state += 10
 }
+Write-Progress -PercentComplete 60 -Activity 'Integration test: Login to Inventory, to create a product' -Status "Docker compose: execute additional steps loginsvc"
+
 
 # additional step LoginService
-docker-compose exec web loginapi db upgrade
+start-process -Wait -FilePath "docker-compose" -ArgumentList @("exec", "web","loginapi","db","upgrade")
+
+#docker-compose exec web loginapi db upgrade
 
 $url = "http://localhost:5000/auth/login"
 
 $body = '
-{
+{: 
  "username": "gebruiker@wmstest.nl",
  "password": "WachtwoordGebruiker1"
 }
 '
 
 #read-host "press key to continue"
-$response = Invoke-RestMethod -Method Post -Uri $url -Body $body -ContentType "application/json" -verbose
+Write-Progress -PercentComplete 75 -Activity 'Integration test: Login to Inventory, to create a product' -Status "send restapi request: retrieve admin login token"
+
+$response = Invoke-RestMethod -Method Post -Uri $url -Body $bodyAdmin -ContentType "application/json" -verbose
 
 #test inventory api create product
 write-host "token is" $response.access_token
 write-host "execute create product test"
+
+start-sleep -Seconds 5
+
 
 $testbody = '
 {
@@ -62,9 +82,12 @@ $testbody = '
 
 $headers = @{"Authorization"=("Bearer "+$response.access_token)}
 $url = "http://localhost:5001/api/v0.1/Products"
+Write-Progress -PercentComplete 90 -Activity 'Integration test: Login to Inventory, to create a product' -Status "send restapi request: create new product using admin token"
 
 $responseProduct = Invoke-RestMethod -Method Put -Uri $url -Body $testbody -ContentType "application/json" -headers $headers -verbose
 Out-String -InputObject $responseProduct
+
+start-sleep -Seconds 20
 
 read-host "press key to stop containers"
 
